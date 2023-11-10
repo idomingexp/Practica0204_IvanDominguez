f5e6fcd3835cd86aa8652dda97b40c799f2d588d
idomingexp <144793269+idomingexp@users.noreply.github.com>
2023-11-10 11:27:31 +0100

    Update Ejercicio 5.py

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,8 +1,9 @@
 d = {}
 n = input("introduce palabra:traducción separándolos por comas:")
 for x in n.split(","):
-    a, b = x.split(":")
-    d[a] = b
+    if x != "terminar":
+        a, b = x.split(":")
+        d[a] = b
 f = input("Introduce una frase en castellano:")
 for x in f.split(" "):
     if x in d:
